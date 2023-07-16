d00a99dcb72d0235d0a877fcfba5f3fdad3c8777
kobo kanaeru <reza.eduwork@gmail.com>
2023-07-16 14:01:03 +0700

    init

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,6 +28,10 @@
         <x-input-error :messages="session('error')" class="mt-2" />
         @endif
 
+        <div style="margin-top:1rem;">
+          <a href="{{url('forgot-password')}}">Lupa Password ?</a>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                 {{ __('Daftar') }}
