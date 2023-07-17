8cf8ba7662d941c00ac48a8717f73ddef7468ee8
kobo kanaeru <reza.eduwork@gmail.com>
2023-07-17 22:30:12 +0700

    dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(\Illuminate\Http\Request $request): void
     {
       if ($request->server->has('HTTP_X_ORIGINAL_HOST')) {
         $this->app['url']->forceRootUrl($request->server->get('HTTP_X_FORWARDED_PROTO').'://'.$request->server->get('HTTP_X_ORIGINAL_HOST'));
